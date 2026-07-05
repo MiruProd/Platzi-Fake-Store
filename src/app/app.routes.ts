7f97815835b6adac1c9d59522a38a7da5910775b
MixerPixel226 <maxuswill@yandex.ru>
2026-07-05 15:40:08 +0700

    Добавлены маршруты для каталога, корзины, профиля

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'products',
+        loadComponent: () => import('./features/catalog/pages/product-list/product-list').then(m => m.ProductList)
+    },
+    {
+        path: 'product/:id',
+        loadComponent: () => import('./features/catalog/pages/product-detail/product-detail').then(m => m.ProductDetail)
+    },
+    {
+        path: 'cart',
+        loadComponent: () => import('./features/cart/pages/cart-page/cart-page').then(m => m.CartPage)
+    },
+    {
+        path: 'profile',
+        loadComponent: () => import('./features/profile/pages/profile-page/profile-page').then(m => m.ProfilePage)
+    },
+    {
+        path: 'auth',
+        loadComponent: () => import('./features/auth/pages/auth-page/auth-page').then(m => m.AuthPage)
+    }
+];
