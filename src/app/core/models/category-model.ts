581e5cd895fb6d5f25a88f0a8a5e831cb5cb874f
MiruProd <225598651+MiruProd@users.noreply.github.com>
2026-07-05 16:57:01 +0700

    Описаны все необходимые модели для взаимодействия с API
    Модели описаны по следующему принципу - есть базовая модель со всеми обязательными полями, которые будут  использоваться в каждой реализации. От базовой модели расширяются модели, которые будут использоваться при приеме или отправке запросов если такой функционал предусмотрен бэкендом.

--- src/app/core/models/category-model.ts
+++ src/app/core/models/category-model.ts
@@ -1,1 +1,25 @@
-export interface CategoryModel {}
+/**
+ * Базовые поля категории, общие для всех сценариев.
+ */
+export interface CategoryBase {
+  name: string;
+  image: string; // URL-ссылка на изображение.
+}
+
+/**
+ * Модель категории для отображения (GET-запросы).
+ */
+export interface CategoryModel extends CategoryBase {
+  id: number; // Генерируется автоматически на сервере.
+  slug: string; // Человекопонятный URL-идентификатор.
+}
+
+/**
+ * ДТО для создания категории (POST-запрос).
+ */
+export interface CreateCategoryDto extends CategoryBase {}
+
+/**
+ * ДТО для обновления категории (PUT-запросы).
+ */
+export type UpdateCategoryDto = Partial<CreateCategoryDto>;
