6d220903ad61841beec6df00b522981f94a63c95
MixerPixel226 <maxuswill@yandex.ru>
2026-07-06 18:38:33 +0700

    Написана директива кнопок с учетом типа, размера

--- src/app/shared/ui-kits/button/button.ts
+++ src/app/shared/ui-kits/button/button.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+
+type ButtonStyle = 'primary' | 'secondary' | 'accent' | 'outlet';
+type ButtonSize = 'sm' | 'md' | 'lg';
 
 @Component({
-  selector: 'app-button',
+  selector: 'button[app-button], a[app-button]',
   imports: [],
   templateUrl: './button.html',
   styleUrl: './button.scss',
+  host: {
+    '[class]': 'hostClasses()',
+  },
 })
-export class Button {}
+export class Button {
+  public readonly variant = input<ButtonStyle>('primary');
+  public readonly disabled = input<boolean>(false);
+  public readonly size = input<ButtonSize>('md');
+
+  protected readonly hostClasses = computed(() => {
+    return [`btn-${this.variant()}`, `btn-${this.size()}`, this.disabled() ? 'is-disabled' : '']
+      .filter(Boolean)
+      .join(' ');
+  });
+}
