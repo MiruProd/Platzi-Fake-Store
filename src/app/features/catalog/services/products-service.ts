cdd9a116efc534c83b47d3fe44316d287c51988f
MixerPixel226 <maxuswill@yandex.ru>
2026-07-07 20:54:42 +0700

    Добавлены методы получения категорий и товаров по ним

--- src/app/features/catalog/services/products-service.ts
+++ src/app/features/catalog/services/products-service.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../../../core/services/api-service';
 import { ProductModel } from '../../../core/models/product-model';
 import { Observable } from 'rxjs';
 import { HttpParams } from '@angular/common/http';
+import { CategoryModel } from '../../../core/models/category-model';
 
 @Injectable({
   providedIn: 'root',
@@ -22,5 +23,29 @@ export class ProductsService {
 
   public getProductById(id: number): Observable<ProductModel> {
     return this.apiService.get<ProductModel>(`/products/${id}`);
+  }
+
+  public getCategories(limit: number): Observable<CategoryModel[]> {
+    const params = new HttpParams({
+      fromObject: {
+        limit,
+      },
+    });
+
+    return this.apiService.get<CategoryModel[]>('/categories', params);
+  }
+
+  public getProductsByCategory(
+    categoryId: number,
+    limit: number,
+    offset: number,
+  ): Observable<ProductModel[]> {
+    const params = new HttpParams({
+      fromObject: {
+        limit,
+        offset,
+      },
+    });
+    return this.apiService.get<ProductModel[]>(`/categories/${categoryId}/products`, params);
   }
 }
