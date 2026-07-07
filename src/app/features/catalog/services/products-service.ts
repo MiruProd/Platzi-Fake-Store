7983b20ce4602943e3c85482155262bb45a0e059
MixerPixel226 <maxuswill@yandex.ru>
2026-07-07 20:42:54 +0700

    Добавлены методы для получения каталога и данных товаров

--- src/app/features/catalog/services/products-service.ts
+++ src/app/features/catalog/services/products-service.ts
@@ -1,6 +1,26 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { ApiService } from '../../../core/services/api-service';
+import { ProductModel } from '../../../core/models/product-model';
+import { Observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductsService {}
+export class ProductsService {
+  private readonly apiService = inject(ApiService);
+
+  public getProducts(limit: number, offset: number): Observable<ProductModel[]> {
+    const params = new HttpParams({
+      fromObject: {
+        limit,
+        offset,
+      },
+    });
+    return this.apiService.get<ProductModel[]>('/products', params);
+  }
+
+  public getProductById(id: number): Observable<ProductModel> {
+    return this.apiService.get<ProductModel>(`/products/${id}`);
+  }
+}
